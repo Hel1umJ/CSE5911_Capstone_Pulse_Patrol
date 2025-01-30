e9217246880c5a3f144c12c6c9a0f6e7a9ba6255
Hel1umJ <brendanisaboss2000@gmail.com>
2025-01-30 13:24:32 -0500

    Finished border and content frame setup

--- LocalGUI/GUILocal.py
+++ LocalGUI/GUILocal.py
@@ -1,21 +1,49 @@
 import tkinter as tk
 from tkinter import ttk
-import time
 
-class App():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.label = tk.Label(text="")
-        self.label.pack()
-        self.update_clock()
-        self.root.mainloop()
+COLORS = {
+    "fg": "black",
+    "red": "red",
+    "highlight": "lightgray",
+    "gray": "gray"
+}
 
-    def update_clock(self):
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(1000, self.update_clock)
+FONTS = {
+    "title": ("Arial", 14, "bold"),
+    "subtitle": ("Arial", 12, "bold"),
+    "label": ("Arial", 10),
+    "bold_label": ("Arial", 10, "bold"),
+    "footer": ("Arial", 10, "italic")
+}
+
+def create_gui():
+    root = tk.Tk()
+    root.title("NORA")
+    root.geometry("800x600")
+    root.configure(bg="white")
+    root.minsize(600, 425)
+
+    #red border frame
+    borderFrame = tk.Frame(root, bg="red", bd=2)
+    borderFrame.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+
+    #main tkinter frame that all widgets will be added to
+    contentFrame = tk.Frame(borderFrame, bg="white")
+    contentFrame.pack(expand=True, fill=tk.BOTH, padx=4, pady=4)
+
+    #set contentFrame to have 5 rows (0–4), 2 columns (0–1); Weight: 0 (dont expand to fill), 1 (expand to fill space)
+    contentFrame.rowconfigure(0, weight=1)
+    contentFrame.rowconfigure(1, weight=1)
+    contentFrame.rowconfigure(2, weight=1)
+    contentFrame.rowconfigure(3, weight=1)
+    contentFrame.rowconfigure(4, weight=1)
+
+    contentFrame.columnconfigure(0, weight=1)  # Vitals (left column)
+    contentFrame.columnconfigure(1, weight=1)  # Patient Info & Flow (right column)
 
 
-if(__name__ == "__main__"):
-    app=App()
+    return root
 
+if __name__ == "__main__":
+    app = create_gui()
+    app.mainloop()
