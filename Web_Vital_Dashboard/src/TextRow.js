d23071eb2d0a2907148e968bb729ce0ccb0199cd
akopelcheck <kopelcheck.1@buckeyemail.osu.edu>
2025-03-04 00:03:14 -0500

    Initial GUI updates

--- Web_Vital_Dashboard/src/TextRow.js
+++ Web_Vital_Dashboard/src/TextRow.js
@@ -3,15 +3,15 @@ import './TextRow.css';
 
 function TextRow({ title, subtitle, rev=false, large=false }) {
   return (
-    <div className="text-row">
+    <div className="text-row ">
         {rev ? (
           <>
             {large ? <h4>{subtitle}</h4> : <p>{subtitle}</p>}
-            <h1>{title}</h1>
+            <h1 className="bloodPressure-sensor-value">{title}</h1>
           </>
         ) : (
           <>
-            <h1>{title}</h1>
+            <h1 className="small-sensor-value">{title}</h1>
             {large ? <h4>{subtitle}</h4> : <p>{subtitle}</p>}
           </>
         )}
